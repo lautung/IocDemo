51b53f4d13e7c8b1d935801ccc6c564edc5ab736
lautung <2281876422@qq.com>
2021-07-29 21:49:06 +0800

    first commit

---
+++ runtimeInject/src/main/java/com/lautung/runtimeInject/ContentView.java
@@ -0,0 +1,21 @@
+package com.lautung.runtimeInject;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)//java-->class-->runtime
+public @interface ContentView {
+    int value();
+}
+
+
+
+
+
+
+
+
+
---
+++ runtimeInject/src/main/java/com/lautung/runtimeInject/EventBase.java
@@ -0,0 +1,33 @@
+package com.lautung.runtimeInject;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.ANNOTATION_TYPE)//比如写在OnClick注解上
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EventBase {
+//    textView.setOnClickListener（new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//
+//        }
+//    });
+    //1.setOnClickListener 订阅关系
+    String listenerSetter();
+    //2.new View.OnClickListener()  事件本身
+    Class<?> listenerType();
+    //3.事件处理程序
+    String callbackMethod();
+
+}
+
+
+
+
+
+
+
+
+
